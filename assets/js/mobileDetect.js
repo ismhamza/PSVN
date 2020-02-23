068843b4e04382d64c8c215529665f50d2680cc6
ismhamza <hamza_dsl@hotmail.com>
2020-02-23 23:41:42 +0500

    media changes

---
+++ assets/js/mobileDetect.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(window).resize(function() {
+      if (window.outerWidth > 768) {
+        $(".psvn-header-menu-icon").click(function(){  
+            $(".app-wrapper").toggleClass("icon-nav");  
+        });
+      }
+      if( window.outerWidth <= 768 ) {
+        
+        $(".psvn-sidebar-wrapper").addClass('mobile');
+        $(".psvn-main-view").addClass('mobile');
+
+        $('.backdrop').click(function() {
+          $('.backdrop').toggleClass('show');  
+          $(".psvn-sidebar-wrapper").css('transform','translateX(-500px)');
+        });
+
+        $(".psvn-header-menu-icon").click(function(){
+            $('.backdrop').toggleClass('show');  
+            $(".psvn-sidebar-wrapper").css({'transform': 'translateX(0)','z-index': '3'});
+        });  
+      }         
+    });    
+  });
