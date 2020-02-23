3e700c0402062286bb03ccb5cf2824ac0351a834
ismhamza <hamza_dsl@hotmail.com>
2020-02-24 00:18:15 +0500

    chnages

--- assets/js/mobileDetect.js
+++ assets/js/mobileDetect.js
@@ -10,14 +10,12 @@ $(document).ready(function() {
         $(".psvn-sidebar-wrapper").addClass('mobile');
         $(".psvn-main-view").addClass('mobile');
 
-        $('.backdrop').click(function() {
+        $('.backdrop').off().on('click',function() {
           $('.backdrop').toggleClass('show');  
           $(".psvn-sidebar-wrapper").css('transform','translateX(-500px)');
         });
-        $(".psvn-header-menu-icon").click(function(){
-                $('.backdrop').toggleClass('show');
-        });
-        $(".psvn-header-menu-icon").click(function(){
+        $(".psvn-header-menu-icon").off().on('click',function(){
+            $('.backdrop').toggleClass('show');
             $(".psvn-sidebar-wrapper").css({'transform': 'translateX(0)','z-index': '3'});
         });  
       }         
