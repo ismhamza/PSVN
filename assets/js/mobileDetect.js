f92e162200e93a3390c5af80f622de8ec7a75a14
ismhamza <hamza_dsl@hotmail.com>
2020-02-24 00:26:39 +0500

    mobile scrren issyue

--- assets/js/mobileDetect.js
+++ assets/js/mobileDetect.js
@@ -1,23 +1,27 @@
-$(document).ready(function() {
-    $(window).resize(function() {
-      if (window.outerWidth > 768) {
-        $(".psvn-header-menu-icon").click(function(){  
-            $(".app-wrapper").toggleClass("icon-nav");  
-        });
-      }
-      if( window.outerWidth <= 768 ) {
-        
-        $(".psvn-sidebar-wrapper").addClass('mobile');
-        $(".psvn-main-view").addClass('mobile');
+function mobileScreenDetect() {
+    if (window.outerWidth > 768) {
+      $(".psvn-header-menu-icon").click(function(){  
+          $(".app-wrapper").toggleClass("icon-nav");  
+      });
+    }
+    if( window.outerWidth <= 768 ) {
+      
+      $(".psvn-sidebar-wrapper").addClass('mobile');
+      $(".psvn-main-view").addClass('mobile');
 
-        $('.backdrop').off().on('click',function() {
-          $('.backdrop').toggleClass('show');  
-          $(".psvn-sidebar-wrapper").css('transform','translateX(-500px)');
-        });
-        $(".psvn-header-menu-icon").off().on('click',function(){
-            $('.backdrop').toggleClass('show');
-            $(".psvn-sidebar-wrapper").css({'transform': 'translateX(0)','z-index': '3'});
-        });  
-      }         
-    });    
+      $('.backdrop').off().on('click',function() {
+        $('.backdrop').toggleClass('show');  
+        $(".psvn-sidebar-wrapper").css('transform','translateX(-500px)');
+      });
+      $(".psvn-header-menu-icon").off().on('click',function(){
+          $('.backdrop').toggleClass('show');
+          $(".psvn-sidebar-wrapper").css({'transform': 'translateX(0)','z-index': '3'});
+      });  
+    }         
+  }
+$(document).ready(function() {
+    mobileScreenDetect();
+    $(window).resize(
+        mobileScreenDetect
+    );    
   });
